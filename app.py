c8a127bcbf2e29fdefdbc5028243615903c077e3
Dongjin Lee <djlee1509@gmail.com>
2020-12-17 15:22:07 +0000

    Completed MVP to return one value per state.

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+import json
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+JSON_FILE_PATH = './data/pv_yield.json'
+
+with open(JSON_FILE_PATH) as f:
+  data = json.load(f)
+
+
+@app.route("/api/pv_yield")
+def get_energy_data():
+  param_state = request.args.get("state")
+
+  for x in data:
+    if x['state'] == param_state:
+      output_dict = x
+      output_json = json.dumps(output_dict)
+  return output_json
 
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
